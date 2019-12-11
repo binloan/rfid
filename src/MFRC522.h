7598a6c34c0a506a7e1fed9887e661ed7a5a82b3
Jonathan Berner <jonathan.berner@egoproducts.com>
2019-12-11 11:26:28 +0100

    updated spi_clock speed to work better

--- src/MFRC522.h
+++ src/MFRC522.h
@@ -84,7 +84,7 @@
 #include <SPI.h>
 
 #ifndef MFRC522_SPICLOCK
-#define MFRC522_SPICLOCK SPI_CLOCK_DIV4			// MFRC522 accept upto 10MHz
+#define MFRC522_SPICLOCK 500000			// MFRC522 accept upto 10MHz
 #endif
 
 // Firmware data for self-test
